95ba07fac7a1af6b105c70943a037ed1fc3cb047
Phil Rzewski <phil@looky.cloud>
2019-12-04 15:14:06 -0800

    Web server for taking profiling

--- cmd/zq/main.go
+++ cmd/zq/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	if _, err := Zq.ExecRoot(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
